5e7ab2b446ce951b3c32ab95edef801d788800be
aolivera <haolivera92@gmail.com>
2019-09-18 22:41:21 -0300

    cambio en base de datos y gitignore

--- application/config/database.php
+++ application/config/database.php
@@ -74,12 +74,12 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	=> '',
+	'dsn'	=> 'mysql:host=localhost;dbname=epixel;charset=utf8',
 	'hostname' => 'localhost',
 	'username' => '',
 	'password' => '',
-	'database' => '',
-	'dbdriver' => 'mysqli',
+	'database' => 'epixel',
+	'dbdriver' => 'pdo',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
